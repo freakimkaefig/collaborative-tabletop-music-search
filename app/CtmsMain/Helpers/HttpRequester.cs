a902e279e5b1977dc1ffbfeca22968ac3b72627c
10ffAppHarbor <10freefingers@gmail.com>
2013-11-27 15:15:11 +0100

    Project created
    WebRequest works. Getting Echo Nest-Response als string. Linked JSON.NET
    Library. No JSON parsing right now

---
+++ app/CtmsMain/Helpers/HttpRequester.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace Helpers
+{
+    public class HttpRequester
+    {
+
+        //http://stackoverflow.com/questions/924679/c-sharp-how-can-i-check-if-a-address-exists-is-valid
+        public static string StartRequest(string address)
+        {
+            var response = "";
+
+            if (address == null)
+            {
+                throw new Exception();
+            }
+
+            // using MyClient from linked post
+            using (var client = new MyClient())
+            {
+                try
+                {
+                    // throws 404 or other exception?
+                    response = client.DownloadString(address);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return response;
+        }
+
+        class MyClient : WebClient
+        {
+            public bool HeadOnly { get; set; }
+            protected override WebRequest GetWebRequest(Uri address)
+            {
+                WebRequest req = base.GetWebRequest(address);
+                if (HeadOnly && req.Method == "GET")
+                {
+                    req.Method = "HEAD";
+                }
+                return req;
+            }
+        }
+    }
+}
