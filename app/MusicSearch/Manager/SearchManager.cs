759664b6aa4633f819274d0fd54a8662ddec5fb3
10freefingers <10freefingers@gmail.com>
2013-12-27 18:24:57 +0100

    Created basic software architecture using the WPF Application Framework (WAF). Example ui-interaction has been implemented as search-button with a default query plus random result.
    Some classes are created just as an example and have to be adjusted. The current view separation is also just an example and have to be adjusted.

--- app/MusicSearch/Manager/SearchManager.cs
+++ app/MusicSearch/Manager/SearchManager.cs
@@ -13,24 +13,65 @@ namespace MusicSearch.Manager
     {
         private String mRequest;
         private String mResponse;
+        public ResponseContainer ResponseContainer;
+        private bool mIsTest = false;
 
         public void Start()
         {
-            //Test request
+            RequestOffline();
+            StartRequest();
+            ParseResponse();
+
+            var firstSong = ResponseContainer.response.songs.First();
+            Console.WriteLine(firstSong.title);
+            Console.WriteLine(firstSong.artist_name);
+        }
+
+        private void StartRequest()
+        {
+            if (mIsTest == false)
+            {   //Do online query
+                LoadOnlineResponse();
+            }
+            else
+            {   //Load offline test response
+                LoadTestResponse();
+            }
+        }
+
+        private void RequestOffline()
+        {
             mRequest = "http://developer.echonest.com/api/v4/song/search?api_key=L5WMCPOK4F2LA9H5X&style=rock&min_danceability=0.65&min_tempo=140&results=5";
+        }
+
+        private void LoadOnlineResponse()
+        {
             //JSON response delivered as string
             mResponse = HttpRequester.StartRequest(mRequest);
-
-            ParseResponse();
+            mResponse = mResponse.Replace("'", "&#39;");
         }
 
         public void ParseResponse()
         {
-            //Continue with: http://james.newtonking.com/json/help/index.html
+            //http://james.newtonking.com/json/help/index.html
             //Escapes in string making problems?
+            var cleared = @"" + mResponse.Replace("\"", "'");//Apostrophes are right now replaced by HTML unicode
+            ResponseContainer = JsonConvert.DeserializeObject<ResponseContainer>(cleared);
+        }
 
-            //Response response = JsonConvert.DeserializeObject<Response>(mResponse);
-            //TestCommit dave
+        public void LoadTestResponse()
+        {
+            mResponse = @"{'response': 
+            {
+            'status': 
+                {'version': '4.2', 'code': 0, 'message': 'Success'}, 
+            'songs': [
+                    {'artist_id': 'AR1R1P51187B9A9AB6', 'id': 'SOKJAGG13134398A08', 'artist_name': 'The Reason', 'title': 'The Reason'}, 
+                    {'artist_id': 'AR1R1P51187B9A9AB6', 'id': 'SOMLHVF137460C1EE7', 'artist_name': 'The Reason', 'title': 'Aint Nuthin'}, 
+                    {'artist_id': 'AR1R1P51187B9A9AB6', 'id': 'SOUUVGW12D8578C479', 'artist_name': 'The Reason', 'title': 'Mirror, Mirror (Talkin Bout Me)'}, 
+                    {'artist_id': 'ARS73SD1187B9971AF', 'id': 'SORXFOX1400C70B29F', 'artist_name': 'Home Grown', 'title': 'Cleopatras Dream'}, 
+                    {'artist_id': 'ARS73SD1187B9971AF', 'id': 'SOXXCYS131343A079A', 'artist_name': 'Home Grown', 'title': 'Complicated'}]}}";
+
         }
     }
 }
