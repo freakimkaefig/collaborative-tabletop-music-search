77e2c2f311d40df30c6cfa4c99a9c853f31f5dee
led13714 <david.lechler@stud.uni-regensburg.de>
2014-01-14 15:54:10 +0100

    Methoden (unfertig) ergänzt.

--- app/MusicSearch/Manager/SearchManager.cs
+++ app/MusicSearch/Manager/SearchManager.cs
@@ -11,16 +11,17 @@ namespace MusicSearch.Manager
 {
     public class SearchManager
     {
-        private String mRequest;
-        private String mResponse;
+        private String _request;
+        private String _response;
+        private String _defaultURL = "http://developer.echonest.com/api/v4/";
         public ResponseContainer ResponseContainer;
         private bool mIsTest = false;
 
         public void Start()
         {
-            RequestOffline();
-            StartRequest();
-            ParseResponse();
+            RequestOffline();   //build query
+            StartRequest();     //send query
+            ParseResponse();    //parse received JSON-Data
 
             var firstSong = ResponseContainer.response.songs.First();
             Console.WriteLine(firstSong.title);
@@ -41,27 +42,42 @@ namespace MusicSearch.Manager
 
         private void RequestOffline()
         {
-            mRequest = "http://developer.echonest.com/api/v4/song/search?api_key=L5WMCPOK4F2LA9H5X&style=rock&min_danceability=0.65&min_tempo=140&results=5";
+            _request = "http://developer.echonest.com/api/v4/song/search?api_key=L5WMCPOK4F2LA9H5X&style=rock&min_danceability=0.65&min_tempo=140&results=5";
+            //_request = BuildString();
+        }
+
+        private void BuildString()
+        {
+            //_defaultURL + 
+            //angesprochener API Teil +
+            //Methodenaufruf + 
+            //String apiKey = GetAPIKey() +
+            //Parameter 
+        }
+
+        private void GetAPIKey()
+        {
+            //get API-Key 
         }
 
         private void LoadOnlineResponse()
         {
             //JSON response delivered as string
-            mResponse = HttpRequester.StartRequest(mRequest);
-            mResponse = mResponse.Replace("'", "&#39;");
+            _response = HttpRequester.StartRequest(_request);
+            _response = _response.Replace("'", "&#39;");
         }
 
         public void ParseResponse()
         {
             //http://james.newtonking.com/json/help/index.html
             //Escapes in string making problems?
-            var cleared = @"" + mResponse.Replace("\"", "'");//Apostrophes are right now replaced by HTML unicode
+            var cleared = @"" + _response.Replace("\"", "'");//Apostrophes are right now replaced by HTML unicode
             ResponseContainer = JsonConvert.DeserializeObject<ResponseContainer>(cleared);
         }
 
         public void LoadTestResponse()
         {
-            mResponse = @"{'response': 
+            _response = @"{'response': 
             {
             'status': 
                 {'version': '4.2', 'code': 0, 'message': 'Success'}, 
