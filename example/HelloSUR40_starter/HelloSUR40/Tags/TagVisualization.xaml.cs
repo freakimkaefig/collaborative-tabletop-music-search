0652ae2b154b7b727528ff8181c53c0835a6df84
led13714 <david.lechler@stud.uni-regensburg.de>
2014-01-14 16:26:09 +0100

    added TagVisualization from Sur40_starter

---
+++ example/HelloSUR40_starter/HelloSUR40/Tags/TagVisualization.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace SurLaRoute.Tags {
+
+    public partial class SimpleVisualization
+    {
+        public SimpleVisualization()
+        {
+            InitializeComponent();
+        }
+
+
+        public void SimpleVisualization_Loaded(object sender, RoutedEventArgs e) 
+    {
+    }
+}
+}
