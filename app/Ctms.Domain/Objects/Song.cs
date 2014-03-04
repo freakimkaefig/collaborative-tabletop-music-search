d33db464163c65b39ea8bec626ada774ad552ba9
10freefingers <10freefingers@gmail.com>
2014-03-04 16:41:30 +0100

    Merged conflicts fullscreen -> dev

--- app/Ctms.Domain/Objects/Song.cs
+++ app/Ctms.Domain/Objects/Song.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+//using MusicSearch.ResponseObjects;
 
 namespace Ctms.Domain.Objects
 {
@@ -9,5 +10,6 @@ namespace Ctms.Domain.Objects
     {
         public string Title { get; set; }
         public string Artist { get; set; }
+        //public ResponseContainer ResponseContainer { get; set; }
     }
 }
