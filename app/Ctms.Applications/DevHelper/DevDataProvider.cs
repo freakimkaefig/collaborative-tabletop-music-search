8fd1afeb3e4526214a0d5acc4062624534bf9601
10freefingers <10freefingers@gmail.com>
2014-03-04 17:00:16 +0100

    Added DevDataProvider

---
+++ app/Ctms.Applications/DevHelper/DevDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ctms.Applications.DevHelper
+{
+    public class DevDataProvider
+    {
+        public void LoadResults()
+        {
+
+        }
+    }
+}
