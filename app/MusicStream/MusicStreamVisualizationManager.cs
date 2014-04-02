8bddd31ca0069b9170c1cf7b23c735fed737b306
led13714 <david.lechler@stud.uni-regensburg.de>
2014-04-02 14:50:11 +0200

    implemented Lomont-FFT and byte[] to double[] conversion for the FFT

--- app/MusicStream/MusicStreamVisualizationManager.cs
+++ app/MusicStream/MusicStreamVisualizationManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using SpotifySharp;
 using System.IO;
+using System.Diagnostics;
 
 namespace MusicStream
 {
@@ -11,6 +12,7 @@ namespace MusicStream
     {
         private MusicStreamSessionManager _sessionManager;
         private int _counter = 0;
+        private Lomont.LomontFFT _lomontFFT;
 
         /// <summary>
         /// Constructor for MusicStreamVisualizationManager
@@ -34,8 +36,28 @@ namespace MusicStream
             SampleType sampleType = format.sample_type;     //SampleType = Int16NativeEndian
 
             AudioBufferStats stats = _sessionManager.GetCurrentAudioBufferStats();
-            int samples = stats.samples;
-            int stutter = stats.stutter;
+            int samples = stats.samples; //Anzahl der abgespielten bzw. gebufferten Bytes
+            int stutter = stats.stutter; //"Hänger"
+
+            Debug.WriteLine("DATA received:\nSamples: "+samples+"\nHänger: "+stutter+"\nFrames: "+frames);
+
+            Double[] preparedFrames = prepareBytes(channels, sampleRate, frames);
+
+            _lomontFFT = new Lomont.LomontFFT();
+            Double[] framesFFT = _lomontFFT.TableFFT(preparedFrames, true);
+        }
+
+        //Source: https://stackoverflow.com/questions/17416112/apply-fft-on-pcm-data-and-convert-to-a-spectrogram 
+
+        public Double[] prepareBytes(int channels, int sampleRate, byte[] frames)
+        {
+            Double[] data = new Double[frames.Length / 4];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = BitConverter.ToInt16(frames, i * 4) / 65536.0;
+            }
+
+            return data;
         }
     }
 }
