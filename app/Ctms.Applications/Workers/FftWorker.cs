3512658335bca23a451266d6aa09a49a62e20fba
led13714 <david.lechler@stud.uni-regensburg.de>
2014-04-03 17:24:48 +0200

    included dispatcher to run animations of EQ-visualization.

--- app/Ctms.Applications/Workers/FftWorker.cs
+++ app/Ctms.Applications/Workers/FftWorker.cs
@@ -18,8 +18,10 @@ namespace Ctms.Applications.Workers
         private MusicStreamSessionManager _sessionManager;
         private MusicStreamVisualizationManager _visualizationManager;
         private SearchViewModel _searchViewModel;
-        private Dispatcher _dp;
-        private int _duration = 200;
+        private double[] _array;
+        public delegate void UpdateDelegate();
+        //private Dispatcher _dp;
+        private int _duration = 250;
         //public Action<MusicStreamVisualizationManager> VisualizationManagerCreated;
 
         [ImportingConstructor]
@@ -37,18 +39,22 @@ namespace Ctms.Applications.Workers
             _sessionManager = sessionManager;
             _visualizationManager = new MusicStreamVisualizationManager(sessionManager);
             _sessionManager.VisualizationManager = _visualizationManager;
-            _visualizationManager.FftDataReceived = UpdateVm;
+            _visualizationManager.FftDataReceived = Dispatch;
         }
 
-        private void UpdateVm(double[] arr)
+        public void Dispatch(double[] arr)
         {
+            _array = arr;
+            Application.Current.Dispatcher.Invoke(new UpdateDelegate(UpdateVm));
+        }
 
-            if (arr.Length >= 6)
+        public void UpdateVm()
+        {
+            if (_array.Length >= 6)
             {
+                _searchViewModel.Fft1Value = (int)Math.Abs(_array[6] * 800);
 
-                _searchViewModel.Fft1Value = (int)Math.Abs(arr[6] * 300);
-
-                //int ms = (int)arr[arr.Length - 1];
+                //int _duration = (int)_array[_array.Length - 1];
 
                 DoubleAnimation interpolate1 = new DoubleAnimation() { From = _searchViewModel.Fft1Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
 
@@ -56,269 +62,218 @@ namespace Ctms.Applications.Workers
 
                 Storyboard.SetTargetProperty(interpolate1, new PropertyPath(System.Windows.Shapes.Rectangle.HeightProperty));
 
-                _dp = Dispatcher.CurrentDispatcher;
-                _dp.BeginInvoke(
-                    new Action(() => {
 
-                        Storyboard sb = new Storyboard();
-                        sb.Children.Add(interpolate1);
-                        sb.Begin();
+                Storyboard sb = new Storyboard();
+                sb.Children.Add(interpolate1);
 
-                    })
-                );
-                
+                sb.Begin();
+            
+
             }
             else
             {
                 _searchViewModel.Fft1Value = 0;
             }
-            if (arr.Length >= 9)
+            if (_array.Length >= 9)
             {
-                _searchViewModel.Fft2Value = (int)Math.Abs(arr[9] * 300);
+                _searchViewModel.Fft2Value = (int)Math.Abs(_array[9] * 800);
 
                 //int ms = (int)arr[arr.Length - 1];
 
-                DoubleAnimation interpolate2 = new DoubleAnimation() { From = _searchViewModel.Fft1Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
+                DoubleAnimation interpolate2 = new DoubleAnimation() { From = _searchViewModel.Fft2Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
 
-                Storyboard.SetTarget(interpolate2, _searchViewModel.FftRectangle[0]);
+                Storyboard.SetTarget(interpolate2, _searchViewModel.FftRectangle[1]);
 
                 Storyboard.SetTargetProperty(interpolate2, new PropertyPath(System.Windows.Shapes.Rectangle.HeightProperty));
 
-                _dp = Dispatcher.CurrentDispatcher;
-                _dp.BeginInvoke(
-                    new Action(() =>
-                    {
 
                         Storyboard sb = new Storyboard();
                         sb.Children.Add(interpolate2);
                         sb.Begin();
 
-                    })
-                );
             }
             else
             {
                 _searchViewModel.Fft2Value = 0;
             }
-            if (arr.Length >= 16)
+            if (_array.Length >= 16)
             {
-                _searchViewModel.Fft3Value = (int)Math.Abs(arr[16] * 300);
+                _searchViewModel.Fft3Value = (int)Math.Abs(_array[16] * 800);
 
                 //int ms = (int)arr[arr.Length - 1];
 
-                DoubleAnimation interpolate3 = new DoubleAnimation() { From = _searchViewModel.Fft1Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
+                DoubleAnimation interpolate3 = new DoubleAnimation() { From = _searchViewModel.Fft3Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
 
-                Storyboard.SetTarget(interpolate3, _searchViewModel.FftRectangle[0]);
+                Storyboard.SetTarget(interpolate3, _searchViewModel.FftRectangle[2]);
 
                 Storyboard.SetTargetProperty(interpolate3, new PropertyPath(System.Windows.Shapes.Rectangle.HeightProperty));
 
-                _dp = Dispatcher.CurrentDispatcher;
-                _dp.BeginInvoke(
-                    new Action(() =>
-                    {
-
+               
                         Storyboard sb = new Storyboard();
                         sb.Children.Add(interpolate3);
                         sb.Begin();
 
-                    })
-                );
             }
             else
             {
                 _searchViewModel.Fft3Value = 0;
             }
-            if (arr.Length >= 23)
+            if (_array.Length >= 23)
             {
-                _searchViewModel.Fft4Value = (int)Math.Abs(arr[23] * 300);
+                _searchViewModel.Fft4Value = (int)Math.Abs(_array[23] * 800);
 
                 //int ms = (int)arr[arr.Length - 1];
 
-                DoubleAnimation interpolate4 = new DoubleAnimation() { From = _searchViewModel.Fft1Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
+                DoubleAnimation interpolate4 = new DoubleAnimation() { From = _searchViewModel.Fft4Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
 
-                Storyboard.SetTarget(interpolate4, _searchViewModel.FftRectangle[0]);
+                Storyboard.SetTarget(interpolate4, _searchViewModel.FftRectangle[3]);
 
                 Storyboard.SetTargetProperty(interpolate4, new PropertyPath(System.Windows.Shapes.Rectangle.HeightProperty));
 
-                _dp = Dispatcher.CurrentDispatcher;
-                _dp.BeginInvoke(
-                    new Action(() =>
-                    {
+                
 
                         Storyboard sb = new Storyboard();
                         sb.Children.Add(interpolate4);
                         sb.Begin();
 
-                    })
-                );
             }
             else
             {
                 _searchViewModel.Fft4Value = 0;
             }
-            if (arr.Length >= 34)
+            if (_array.Length >= 34)
             {
-                _searchViewModel.Fft5Value = (int)Math.Abs(arr[34] * 300);
+                _searchViewModel.Fft5Value = (int)Math.Abs(_array[34] * 800);
 
                 //int ms = (int)arr[arr.Length - 1];
 
-                DoubleAnimation interpolate5 = new DoubleAnimation() { From = _searchViewModel.Fft1Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
+                DoubleAnimation interpolate5 = new DoubleAnimation() { From = _searchViewModel.Fft5Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
 
-                Storyboard.SetTarget(interpolate5, _searchViewModel.FftRectangle[0]);
+                Storyboard.SetTarget(interpolate5, _searchViewModel.FftRectangle[4]);
 
                 Storyboard.SetTargetProperty(interpolate5, new PropertyPath(System.Windows.Shapes.Rectangle.HeightProperty));
 
-                _dp = Dispatcher.CurrentDispatcher;
-                _dp.BeginInvoke(
-                    new Action(() =>
-                    {
+                
 
                         Storyboard sb = new Storyboard();
                         sb.Children.Add(interpolate5);
                         sb.Begin();
 
-                    })
-                );
             }
             else
             {
                 _searchViewModel.Fft5Value = 0;
             }
-            if (arr.Length >= 70)
+            if (_array.Length >= 70)
             {
-                _searchViewModel.Fft6Value = (int)Math.Abs(arr[70] * 300);
+                _searchViewModel.Fft6Value = (int)Math.Abs(_array[70] * 800);
 
                 //int ms = (int)arr[arr.Length - 1];
 
-                DoubleAnimation interpolate6 = new DoubleAnimation() { From = _searchViewModel.Fft1Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
+                DoubleAnimation interpolate6 = new DoubleAnimation() { From = _searchViewModel.Fft6Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
 
-                Storyboard.SetTarget(interpolate6, _searchViewModel.FftRectangle[0]);
+                Storyboard.SetTarget(interpolate6, _searchViewModel.FftRectangle[5]);
 
                 Storyboard.SetTargetProperty(interpolate6, new PropertyPath(System.Windows.Shapes.Rectangle.HeightProperty));
 
-                _dp = Dispatcher.CurrentDispatcher;
-                _dp.BeginInvoke(
-                    new Action(() =>
-                    {
+               
 
                         Storyboard sb = new Storyboard();
                         sb.Children.Add(interpolate6);
                         sb.Begin();
 
-                    })
-                );
             }
             else
             {
                 _searchViewModel.Fft6Value = 0;
             }
-            if (arr.Length >= 116)
+            if (_array.Length >= 116)
             {
-                _searchViewModel.Fft7Value = (int)Math.Abs(arr[116] * 300);
+                _searchViewModel.Fft7Value = (int)Math.Abs(_array[116] * 800);
 
                 //int ms = (int)arr[arr.Length - 1];
 
-                DoubleAnimation interpolate7 = new DoubleAnimation() { From = _searchViewModel.Fft1Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
+                DoubleAnimation interpolate7 = new DoubleAnimation() { From = _searchViewModel.Fft7Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
 
-                Storyboard.SetTarget(interpolate7, _searchViewModel.FftRectangle[0]);
+                Storyboard.SetTarget(interpolate7, _searchViewModel.FftRectangle[6]);
 
                 Storyboard.SetTargetProperty(interpolate7, new PropertyPath(System.Windows.Shapes.Rectangle.HeightProperty));
 
-                _dp = Dispatcher.CurrentDispatcher;
-                _dp.BeginInvoke(
-                    new Action(() =>
-                    {
+                
 
                         Storyboard sb = new Storyboard();
                         sb.Children.Add(interpolate7);
                         sb.Begin();
 
-                    })
-                );
             }
             else
             {
                 _searchViewModel.Fft7Value = 0;
             }
-            if (arr.Length >= 163)
+            if (_array.Length >= 163)
             {
-                _searchViewModel.Fft8Value = (int)Math.Abs(arr[163] * 300);
+                _searchViewModel.Fft8Value = (int)Math.Abs(_array[163] * 800);
 
                 //int ms = (int)arr[arr.Length - 1];
 
-                DoubleAnimation interpolate8 = new DoubleAnimation() { From = _searchViewModel.Fft1Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
+                DoubleAnimation interpolate8 = new DoubleAnimation() { From = _searchViewModel.Fft8Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
 
-                Storyboard.SetTarget(interpolate8, _searchViewModel.FftRectangle[0]);
+                Storyboard.SetTarget(interpolate8, _searchViewModel.FftRectangle[7]);
 
                 Storyboard.SetTargetProperty(interpolate8, new PropertyPath(System.Windows.Shapes.Rectangle.HeightProperty));
 
-                _dp = Dispatcher.CurrentDispatcher;
-                _dp.BeginInvoke(
-                    new Action(() =>
-                    {
+               
 
                         Storyboard sb = new Storyboard();
                         sb.Children.Add(interpolate8);
                         sb.Begin();
 
-                    })
-                );
             }
             else
             {
                 _searchViewModel.Fft8Value = 0;
             }
-            if (arr.Length >= 232)
+            if (_array.Length >= 232)
             {
-                _searchViewModel.Fft9Value = (int)Math.Abs(arr[232] * 300);
+                _searchViewModel.Fft9Value = (int)Math.Abs(_array[232] * 800);
 
                 //int ms = (int)arr[arr.Length - 1];
 
-                DoubleAnimation interpolate9 = new DoubleAnimation() { From = _searchViewModel.Fft1Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
+                DoubleAnimation interpolate9 = new DoubleAnimation() { From = _searchViewModel.Fft9Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
 
-                Storyboard.SetTarget(interpolate9, _searchViewModel.FftRectangle[0]);
+                Storyboard.SetTarget(interpolate9, _searchViewModel.FftRectangle[8]);
 
                 Storyboard.SetTargetProperty(interpolate9, new PropertyPath(System.Windows.Shapes.Rectangle.HeightProperty));
 
-                _dp = Dispatcher.CurrentDispatcher;
-                _dp.BeginInvoke(
-                    new Action(() =>
-                    {
+             
 
                         Storyboard sb = new Storyboard();
                         sb.Children.Add(interpolate9);
                         sb.Begin();
 
-                    })
-                );
             }
             else
             {
                 _searchViewModel.Fft9Value = 0;
             }
-            if (arr.Length >= 372)
+            if (_array.Length >= 372)
             {
-                _searchViewModel.Fft10Value = (int)Math.Abs(arr[372] * 300);
+                _searchViewModel.Fft10Value = (int)Math.Abs(_array[372] * 800);
 
                 //int ms = (int)arr[arr.Length - 1];
 
-                DoubleAnimation interpolate10 = new DoubleAnimation() { From = _searchViewModel.Fft1Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
+                DoubleAnimation interpolate10 = new DoubleAnimation() { From = _searchViewModel.Fft10Value, To = 0, Duration = TimeSpan.FromMilliseconds(_duration) };
 
-                Storyboard.SetTarget(interpolate10, _searchViewModel.FftRectangle[0]);
+                Storyboard.SetTarget(interpolate10, _searchViewModel.FftRectangle[9]);
 
                 Storyboard.SetTargetProperty(interpolate10, new PropertyPath(System.Windows.Shapes.Rectangle.HeightProperty));
 
-                _dp = Dispatcher.CurrentDispatcher;
-                _dp.BeginInvoke(
-                    new Action(() =>
-                    {
+               
 
                         Storyboard sb = new Storyboard();
                         sb.Children.Add(interpolate10);
                         sb.Begin();
 
-                    })
-                );
             }
             else
             {
