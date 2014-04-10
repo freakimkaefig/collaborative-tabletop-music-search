2e230ff411eae48e463194e05d5c91888ee36c37
freakimkaefig <lukas.lamm89@googlemail.com>
2014-04-10 18:28:20 +0200

    added event handler for resizing result items

---
+++ app/Ctms.Presentation/Resources/ResultStyleResources.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Ctms.Applications.DataModels;
+
+namespace Ctms.Presentation.Resources
+{
+    public partial class ResultStyleResources
+    {
+        private void ResultWrapper_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            FrameworkElement scatterViewItem = e.Source as FrameworkElement;
+            ResultDataModel result = scatterViewItem.DataContext as ResultDataModel;
+
+            if (e.PreviousSize.Height != 0.0 && e.PreviousSize.Width != 0.0)
+            {
+                if (e.NewSize.Width == result.StdWidth || e.NewSize.Height == result.StdHeight)
+                {
+
+                }
+                else
+                {
+                    //not initial rendering
+                    if (e.NewSize.Width > e.PreviousSize.Width || e.NewSize.Height > e.PreviousSize.Height)
+                    {
+                        //item is getting bigger
+                    }
+                    else
+                    {
+                        //item is getting smaller
+                    }
+                }
+            }
+            else
+            {
+                //initial rendering | save items standard width/height
+                result.StdWidth = e.NewSize.Width;
+                result.StdHeight = e.NewSize.Height;
+            }
+        }
+    }
+}
