480fcaa626795a623865a94c6f061d65ffc150d5
freakimkaefig <lukas.lamm89@googlemail.com>
2014-04-29 18:32:28 +0200

    started with DetailView

---
+++ app/Ctms.Presentation/Converters/ListToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Data;
+
+namespace Ctms.Presentation.Converters
+{
+    public class ListToStringConverter : IValueConverter
+    {
+        private static readonly ListToStringConverter defaultInstance = new ListToStringConverter();
+
+        public static ListToStringConverter Default { get { return defaultInstance; } }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(String))
+                throw new InvalidOperationException("The target must be a String");
+
+            return String.Join(", ", ((List<String>)value).ToArray());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
