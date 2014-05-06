e9fb0b9e4d6dde06d261573c8f9cd8a82e58a010
freakimkaefig <lukas.lamm89@googlemail.com>
2014-05-06 13:05:38 +0200

    logging response from search

--- app/Ctms.Applications/Workers/ResultWorker.cs
+++ app/Ctms.Applications/Workers/ResultWorker.cs
@@ -12,6 +12,7 @@ using SpotifySharp;
 using MusicStream;
 using System.Collections.ObjectModel;
 using Helpers;
+using Ctms.Applications.DevHelper;
 
 namespace Ctms.Applications.Workers
 {
@@ -59,6 +60,7 @@ namespace Ctms.Applications.Workers
                     _resultViewModel.Results.Clear();
                     for (int i = 0; i < response.Count; i++)
                     {
+                        DevLogger.Log("Received Track: " + response[i].Artist_Name + " | " + response[i].Title + " | Origin:" + response[i].originIDs + "\n");
                         if (_resultViewModel.Results.Count < 10)
                         {
                             for (int j = 0; j < response[i].tracks.Count; j++)
